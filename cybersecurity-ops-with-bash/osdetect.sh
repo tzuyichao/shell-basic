212e7c9ec85962d75401a2cab9194ec0271b34b7
TzuYi Chao <tzuyichao0313@gmail.com>
2022-04-11 13:30:29 +0800

    osdetect.sh

---
+++ cybersecurity-ops-with-bash/osdetect.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -
+# Cybersecurity Ops with bash
+# osdetect.sh
+
+if type -t wevtutil &> /dev/null
+then
+  OS=MSWin
+elif type -t scutil &> /dev/null
+then
+  OS=macOS
+else
+  OS=Linux
+fi
+echo $OS
