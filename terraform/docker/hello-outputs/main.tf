0b4e5881e8617960cef97b5c1a62a5eade0d851b
TzuYi Chao <tzuyichao0313@gmail.com>
2022-09-02 16:42:15 +0800

    with outputs

---
+++ terraform/docker/hello-outputs/main.tf
@@ -0,0 +1,26 @@
+terraform {
+    required_providers {
+        docker = {
+            source = "kreuzwerker/docker"
+            version = ">= 2.20.3"
+        }
+    }
+}
+
+provider "docker" {
+    host = "npipe:////.//pipe//docker_engine"
+}
+
+resource "docker_image" "nginx" {
+    name         = "nginx:latest@sha256:89020cd33be2767f3f894484b8dd77bc2e5a1ccc864350b92c53262213257dfc"
+    keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+    image = docker_image.nginx.repo_digest
+    name = var.container_name
+    ports {
+        internal = 80
+        external = 8088
+    }
+}
