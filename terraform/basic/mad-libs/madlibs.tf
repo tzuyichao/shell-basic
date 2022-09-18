3a26947e16004dbf9e023352f11b17e214fbd9e8
TzuYi Chao <tzuyichao0313@gmail.com>
2022-09-18 09:20:41 +0800

    mad libs part 1

---
+++ terraform/basic/mad-libs/madlibs.tf
@@ -0,0 +1,50 @@
+terraform {
+    required_version = ">= 0.15"
+    required_providers {
+        random = {
+            source = "hashicorp/random"
+            version = "~> 3.0"
+        }
+    }
+}
+
+variable "words" {
+    description = "A word pool to use for Mad Libs"
+    type = object({
+        nouns = list(string),
+        adjectives = list(string),
+        verbs = list(string),
+        adverbs = list(string),
+        numbers = list(number),
+    })
+}
+
+resource "random_shuffle" "random-nouns" {
+    input = var.words["nouns"]
+}
+
+resource "random_shuffle" "random-adjectives" {
+    input = var.words["adjectives"]
+}
+
+resource "random_shuffle" "random-verbs" {
+    input = var.words["verbs"]
+}
+
+resource "random_shuffle" "random-adverbs" {
+    input = var.words["adverbs"]
+}
+
+resource "random_shuffle" "random-numbers" {
+    input = var.words["numbers"]
+}
+
+output "mad_libs" {
+    value = templatefile("${path.module}/templates/alice.txt", {
+        nouns = random_shuffle.random-nouns.result
+        adjectives = random_shuffle.random-adjectives.result
+        verbs = random_shuffle.random-verbs.result
+        adverbs = random_shuffle.random-adverbs.result
+        numbers = random_shuffle.random-numbers.result
+    })
+}
