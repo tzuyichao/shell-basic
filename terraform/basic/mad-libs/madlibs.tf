9525fc63281dc925847af091f69e6e1d8280c10c
TzuYi Chao <tzuyichao0313@gmail.com>
2022-09-20 09:04:55 +0800

    archive generate resource

--- terraform/basic/mad-libs/madlibs.tf
+++ terraform/basic/mad-libs/madlibs.tf
@@ -69,3 +69,10 @@ resource "local_file" "mad_libs" {
     })
 }
 
+data "archive_file" "mad_libs" {
+    depends_on = [local_file.mad_libs]
+    type = "zip"
+    source_dir = "${path.module}/madlibs"
+    output_path = "${path.cwd}/madlibs.zip"
+}
+
