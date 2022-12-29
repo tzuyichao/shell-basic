160e980395955e7f9b0a040fbdef287fb63ec5e4
TzuYi Chao <tzuyichao0313@gmail.com>
2022-12-29 14:37:12 +0800

    flow control

--- powershell/microsoft-learn/Backup.ps1
+++ powershell/microsoft-learn/Backup.ps1
@@ -3,6 +3,15 @@ Param(
     [string]$DestinationPath = './'
 )
 
-$date = Get-Date -format "yyyy-MM-dd"
-Compress-Archive -Path $Path -CompressionLevel 'Fastest' -DestinationPath "$($DestinationPath + 'backup-' + $date)"
-Write-Host "Created backup at $('./backup-' + $date + '.zip')"
+IF (-Not (Test-Path $Path)) {
+    Throw "The source directory $Path does not exist, please specify an existing directory."
+} Else {
+    $date = Get-Date -format "yyyy-MM-dd"
+    $DestinationFile = "$($DestinationPath + 'backup-' + $date + '.zip')"
+    If (-Not (Test-Path $DestinationFile)) {
+        Compress-Archive -Path $Path -CompressionLevel 'Fastest' -DestinationPath $DestinationFile
+        Write-Host "Created backup at $DestinationFile"
+    } Else {
+        Write-Error "Today's backup already exists"
+    }
+}
