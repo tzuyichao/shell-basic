af154c8d584bc510f564462df3d0be636968cf8e
terence <tzuyichao0313@gmail.com>
2023-09-24 17:02:00 +0800

    step 2

--- cybersecurity-ops-with-bash/fd2.sh
+++ cybersecurity-ops-with-bash/fd2.sh
@@ -13,6 +13,27 @@ function NotInList()
 }
 
 while true
-    read aline
 do
+    read aline <&4 || break
+    read bline <&5 || break
+    # echo $aline
+    # echo $bline
+    [[ $aline == $bline ]] && continue;
+    HOSTA=${aline%% *}
+    PORTSA=( ${aline#* })
+    HOSTB=${bline%% *}
+    PORTSB=( ${bline#* })
+    echo $HOSTA
+    #echo $PORTSA
+    #echo $HOSTB
+    #echo $PORTSB
+    for porta in ${PORTSA[@]}
+    do
+        LOOKFOR=$porta NotInList ${PORTSB[@]} && echo " closed: $porta"
+    done
+    
+    for portb in ${PORTSB[@]}
+    do
+        LOOKFOR=$portb NotInList ${PORTSA[@]} && echo " new: $portb"
+    done
 done 4< ${1:-day1.data} 5< ${2:-day2.data}
